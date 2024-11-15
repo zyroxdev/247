74c4fa5924ce631d0755c0b2899d164613962bfc
zyroxdev <ameenop007@gmail.com>
2024-11-15 19:14:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const { Client } = require('discord.js-selfbot-v13');
 
-// Create two clients for two accounts
-const account1 = new Client();
-const account2 = new Client();
+// Create two clients for two accounts with PC-like settings
+const account1 = new Client({
+    ws: {
+        properties: {
+            $browser: 'Discord Chrome', // Emulates a desktop environment (Chrome on PC)
+        },
+    },
+});
+const account2 = new Client({
+    ws: {
+        properties: {
+            $browser: 'Discord Chrome', // Emulates a desktop environment (Chrome on PC)
+        },
+    },
+});
 
 // Retrieve tokens from environment variables
 const TOKEN_ACCOUNT1 = process.env.TOKEN_ACCOUNT1;
@@ -11,7 +23,7 @@ const TOKEN_ACCOUNT2 = process.env.TOKEN_ACCOUNT2;
 // Function to log when the account is ready
 function setupAccount(client, accountName) {
     client.on('ready', () => {
-        console.log(`${accountName} (${client.user.tag}) is now online!`);
+        console.log(`${accountName} (${client.user.tag}) is now online with PC badge!`);
     });
 }
 
